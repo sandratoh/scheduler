393e16882fba5cf001c5887657eea8ca0398f674
Sandra To <sandra.to.e@gmail.com>
2021-03-15 18:26:14 -0700

    Format DayListItem based on spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,6 +3,16 @@ import classnames from 'classnames';
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const formatSpots = props => {
+    let num;
+    let unit = 'spot';
+
+    props.spots === 0 ? num = 'no' : num = props.spots;
+    if (props.spots !== 1) {unit += 's'};
+    
+    return `${num} ${unit} remaining`;
+  }
+
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
@@ -11,7 +21,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
