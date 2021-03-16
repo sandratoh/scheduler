ba1cd14be301a37331f2b34eca4518d1db96baf3
Sandra To <sandra.to.e@gmail.com>
2021-03-16 16:40:28 -0700

    Create empty child component to appointment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'components/Appointment/styles.scss';
 
 import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
 
 export default function Appointment(props) {
 
