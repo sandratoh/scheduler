c18c78362dc855d33c2cb5d77934b40fa6ecc959
Sandra To <sandra.to.e@gmail.com>
2021-03-16 16:33:42 -0700

    Create header component to appointment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'components/Appointment/styles.scss';
 
+import Header from 'components/Appointment/Header';
+
 export default function Appointment(props) {
 
   return (
