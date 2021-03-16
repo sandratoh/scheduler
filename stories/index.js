96833be9006baa1ed4ca444b0cfc121290673ba5
Sandra To <sandra.to.e@gmail.com>
2021-03-16 16:53:16 -0700

    Create confirm child component to appointment

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -148,5 +149,12 @@ storiesOf("Appointment", module)
       interviewer={interviewers[2].name}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment?"
+      onCancel={action("onCancel")}
+      onConfirm={action("onConfirm")}
     />
   ))
