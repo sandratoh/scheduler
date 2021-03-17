cd402a373d83260fe0a0e73695b27df937d166c5
Sandra To <sandra.to.e@gmail.com>
2021-03-16 17:04:00 -0700

    Add error child component to appointment

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -160,3 +161,9 @@ storiesOf("Appointment", module)
     />
   ))
   .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => (
+    <Error 
+      message="Could not delete appointment"
+      onClose={action("onClose")}
+    />
+  ))
