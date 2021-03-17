0b6064d0b8fd2a58c32dbca9a22669ffd03774e3
Sandra To <sandra.to.e@gmail.com>
2021-03-16 20:25:24 -0700

    Display appointment with various components

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -4,14 +4,18 @@ import 'components/Appointment/styles.scss';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
-import Confirm from 'components/Appointment/Confirm';
-import Status from 'components/Appointment/Status';
-import Error from 'components/Appointment/Error';
-import Form from 'components/Appointment/Form';
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview
+        ? <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        : <Empty />}
+    </article>
   );
 }
