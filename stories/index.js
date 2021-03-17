0de1d297d0fd98ce23b87d9ef193e36e13894c65
Sandra To <sandra.to.e@gmail.com>
2021-03-16 17:53:36 -0700

    Working codes hard code form child component to appointment

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -165,5 +166,21 @@ storiesOf("Appointment", module)
     <Error 
       message="Could not delete appointment"
       onClose={action("onClose")}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form
+      name="Archie Cohen"
+      interviewers={interviewers}
+      interviewer={interviewers[2].id}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
