66c540f7f3be694a1257aea0babc8893165be609
Sandra To <sandra.to.e@gmail.com>
2021-03-19 17:01:43 -0700

    Remove commented out notes

--- src/hooks/useApplicationData.jsx
+++ src/hooks/useApplicationData.jsx
@@ -2,18 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { updateSpots } from 'helpers/selectors';
 
-// Implementing Spots:
-// 1. Where is the value of 'spots' stored for each day?
-// - CORRECTION: inside state.days, then inside each object has 'spots' key
-
-// 2. When should that value change?
-// - at the same time the states change from bookingInterview / cancelInterview 
-
-// 3. How can we calculate how many spots should be available?
-// Update spots with state.days (answer 1) -1 if creating appt, +1 if deleting appt
-// (Overcomplicated) - From appointment id => interview data => if interview is null then add to spot counter?
-// (Overcomplicated) - Alternatively, if interview is not null, then subtract from 5 (max interview spots allowed)
-
 export default function useApplicationData() {
   const [state, setState] = useState({
     day: 'Monday',
--- stories/index.js
+++ stories/index.js
@@ -40,13 +40,11 @@ storiesOf("DayListItem", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   })
-  // To define our stories, we call add() once for each of our test states to generate a story
   .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
   .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
   .add("Full", () => <DayListItem name="Monday" spots={0} />)
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
-    // action() allows us to create a callback that appears in the actions panel when clicked
   ));
 
 const days = [
