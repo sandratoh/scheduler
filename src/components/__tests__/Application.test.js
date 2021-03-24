744378b2cf1c6a4ed7ebeeb8c077c814b1e21485
Sandra To <sandra.to.e@gmail.com>
2021-03-23 21:27:37 -0700

    Add pseudo codes for error handling tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
@@ -69,40 +70,96 @@ describe("Application", () => {
   });
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment")
+      .find(appointment => queryByText(appointment, "Archie Cohen"));
+
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Ai Dubble" }
+    })
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving...")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Ai Dubble"));
+    
+    expect(getByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
+
+    const day = getAllByTestId(container, "day")
+      .find(day => queryByText(day, "Monday"));
+      
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+  });
+
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce();
+
     // 1. Render the Application.
     const { container, debug } = render(<Application />);
 
     // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
-    // 3. Click the "Edit" button on the first booked appointment.
-    const appointment = getAllByTestId(container, "appointment")
-      .find(appointment => queryByText(appointment, "Archie Cohen"));
+    // 3. Find first available appointment
 
-    fireEvent.click(getByAltText(appointment, "Edit"));
-
-    // 4. Change appointment details.
-    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-      target: { value: "Ai Dubble" }
-    })
-
-    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-    // 5. Click the "Save" button on the same element
-    fireEvent.click(getByText(appointment, "Save"));
-
-    // // 6. Check that the element with the text "Saving..." is displayed.
-    expect(getByText(appointment, "Saving...")).toBeInTheDocument();
-
-    // // 7. Wait until the element with the text "Ai Dubble" is displayed
-    await waitForElement(() => getByText(appointment, "Ai Dubble"));
     
-    expect(getByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
+    // 4. Enter appointment detail
 
-    // // 8. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
-    const day = getAllByTestId(container, "day")
-      .find(day => queryByText(day, "Monday"));
-      
-    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+
+    // 5. Click on "Save" button
+
+
+    // 6. Expect "Saving..." component to display
+
+
+    // 7. Expect error message to show up.
+
+
+    // 8. Click on close button
+
+
+    // 9. Expect to go back to previous page with empty form
+
+
+    // 10. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
+
+    debug();
+  });
+
+  it("shows the delete error when failing to delete an appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Find first booked appointment
+
+    
+    // 4. Click on "Delete" button
+
+
+    // 5. Expect "Deleting..." component to display
+
+
+    // 6. Expect error message to show up.
+
+
+    // 7. Click on close button
+
+
+    // 8. Expect to go back to previous page with appointment details
+
+    // 9. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
 
     debug();
   });
